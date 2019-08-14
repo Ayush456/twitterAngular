24dcd2bc3e841e14528570849272d22b9dcc8163
Aditya Baranwal <baranwal.adi@gmail.com>
2019-08-14 11:42:07 +0530

    conflicts

--- src/app/components/edit/edit-profile.component.ts
+++ src/app/components/edit/edit-profile.component.ts
@@ -44,7 +44,6 @@ export class EditProfileComponent {
         if(this.user_status != this.user_status_old) body.userStatus = this.user_status;
         if(this.user_email != this.user_email_old) body.userEmail = this.user_email;
 
-<<<<<<< HEAD
         this.rest.updateProfile(body).subscribe(
             data => {
                 console.log(data);
@@ -54,29 +53,6 @@ export class EditProfileComponent {
                 console.log(error);
             }
         );
-=======
-        if (this.user_dob == null) {
-            let new_formated_date = (this.temp_dob.getFullYear()) + "-" + (this.temp_dob.getMonth()+1 )+"-"+(this.temp_dob.getDate());
-            console.log(new_formated_date);
-            
-            let body2 ={
-                userId: user_id,
-                user_status : this.user_status,
-                user_dob : new_formated_date,
-                user_email : this.user_email
-            }
-            this.rest.edit_p(body2).subscribe(
-                data => {
-                    localStorage.setItem('user_email', this.user_email);
-                    console.log(data);
-                    this.router.navigate(['/home']);
-                },
-                error => {
-                    console.log(error);
-                }
-            );
-        }
->>>>>>> b803cb265503a911f0eea33293e9f80bf0dfe578
     }
 
     ngOnInit(): void {
